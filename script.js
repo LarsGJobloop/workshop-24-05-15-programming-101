78f6f5ec95f8043b9cd4f1b7a5b2a8d772c0e0a3
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-15 20:57:23 +0200

    add examples for programming 101

--- script.js
+++ script.js
@@ -50,3 +50,90 @@ const todo = {
     isComplete: false,
 }
 
+// Du kan utføre matemetikk på variablene
+const numberA = 10
+const numberB = 32
+const numberC = numberA * numberB + 10
+console.log(numberC)
+
+// Strenger kan og kombineres
+const stringA = "Foo"
+const stringB = "Bar"
+const stringC = stringA + stringB
+console.log(stringC)
+
+// String Templates
+const personName = "Lars"
+const greeting = `Greeting ${personName}!!!`
+console.log(greeting)
+
+// Boolean Operasjoner
+const numberD = 10
+const numberE = 15
+const isGreater = numberD > numberE
+console.log(isGreater)
+
+const nameA = "Lars"
+const nameB = "Lars"
+const isEqual = nameA === nameB
+console.log(isEqual)
+
+// Forgreninger
+
+// if else
+
+const personA = {
+    name: "Kai",
+    age: 17,
+}
+
+if (personA.age > 18) {
+    console.log(`${personA.name} is old enough to enter`)
+} else {
+    console.log(`${personA.name} need to wait some years`)
+}
+
+console.log("if else block finsihed")
+
+// Løkker
+let count = 1
+while (count <= 10) {
+    console.log(count)
+    count = count + 1
+}
+
+for (let count = 1; count <= 10; count++) {
+    console.log(count)
+}
+
+const names = [
+    "Lars",
+    "Kai",
+    "Julie",
+    "Fredrik",
+    "Janus",
+]
+
+console.log(names.length)
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index])
+}
+
+for (const name of names) {
+    console.log(name)
+}
+
+// Funksjoner og Moduler
+function greetPerson(name) {
+    console.log("Greeting " + name)
+}
+greetPerson("Lars")
+
+function multiple(numberA, numberB) {
+    const result = numberA * numberB
+    return result
+}
+
+const resultA = multiple(10, 20)
+console.log(resultA)
